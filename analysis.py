7bf50d1f871e1118f9eaf220832354a043b8302a
Reese Levine <reeselevine1@gmail.com>
2022-02-24 15:35:30 -0800

    First two results

--- analysis.py
+++ analysis.py
@@ -1,17 +1,129 @@
 import json
+import csv
 import argparse
+import pandas
+from scipy import stats
+import matplotlib.pyplot as plt
+
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_rows', None)
+
+def analyze(tests, file_path):
+    df = pandas.read_csv(file_path + '.csv')
+    #print(df.describe())
+    for test in tests:
+        df.hist(column=test)
+    #print(df.corr())
+
 
 def load_stats(stats_path):
-     with open(stats_path, "r") as stats_file:
-         stats = json.loads(stats_file.read())
-         return stats
+    """
+    Load the file with the test run output
+    """
+    with open(stats_path + ".json", "r") as stats_file:
+        dataset = json.loads(stats_file.read())
+        return dataset
+
+def get_tests(dataset):
+    tests = []
+    for key in dataset["0"]:
+        if key != "params":
+            tests.append(key)
+    return tests
+
+def convert_to_csv(dataset, file_path):
+    """
+    Converts a stats json file to a csv, for use with python data science modules.
+    """
+    data_file = open(file_path + ".csv", 'w')
+    csv_writer = csv.writer(data_file)
+    first = True
+    for key in dataset:
+        if key != "randomSeed":
+            row = []
+            if first:
+                for testKey in dataset[key]:
+                    if testKey == "params":
+                        row += dataset[key][testKey].keys()
+                    else:
+                        row.append(testKey)
+                first = False
+                csv_writer.writerow(row)
+                row = []
+            for testKey in dataset[key]:
+                if testKey == "params":
+                    for param, value in dataset[key][testKey].items():
+                        row.append(value)
+                else:
+                    row.append(dataset[key][testKey]["weak"])
+            csv_writer.writerow(row)
+    data_file.close()
+
+def per_test(dataset, compare, update):
+    """
+    General purpose function for calculating some basic statistics per test
+    """
+    result = {}
+    for key in dataset:
+        if key != "randomSeed":
+            for testKey in dataset[key]:
+                if testKey != "params":
+                    if testKey not in result:
+                        update(result, key, testKey)
+                    elif compare(result, key, testKey):
+                        update(result, key, testKey)
+    return result
+
+
+def max_per_test(dataset):
+    """
+    Finds the max number of weak behaviors per test in a test run
+    """
+    def compare(result, key, testKey):
+        return result[testKey][1] < dataset[key][testKey]["weak"]
+    def update(result, key, testKey):
+        result[testKey] = (key, dataset[key][testKey]["weak"])
+
+    result = per_test(dataset, compare, update)
+    print("Max weak behaviors:")
+    for key in result:
+        print(key + ": " + str(result[key][1]) + " in iteration " + result[key][0])
+
+def max_rate_per_test(dataset):
+    """
+    Finds the max rate of weak behaviors per test
+    """
+    def compare(result, key, testKey):
+        value = dataset[key][testKey]["weak"]
+        time = dataset[key][testKey]["durationSeconds"]
+        rate = value/time
+        return result[testKey][1] < rate
+    def update(result, key, testKey):
+        value = dataset[key][testKey]["weak"]
+        time = dataset[key][testKey]["durationSeconds"]
+        rate = value/time
+        result[testKey] = (key, rate)
+
+    result = per_test(dataset, compare, update)
+    print("Rate of weak behaviors:")
+    for key in result:
+        print(key + ": " + str(result[key][1]) + " weak behaviors per second in iteration " + result[key][0])
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("stats_path", help="Path to output to analyze")
     args = parser.parse_args()
-    stats = load_stats(args.stats_path)
-    print(stats["0"])
+    dataset = load_stats(args.stats_path)
+    tests = get_tests(dataset)
+    max_per_test(dataset)
+    print()
+    max_rate_per_test(dataset)
+    print()
+    convert_to_csv(dataset, args.stats_path)
+    #analyze(tests, args.stats_path)
+    #plt.show()
+
 
 if __name__ == "__main__":
     main()
